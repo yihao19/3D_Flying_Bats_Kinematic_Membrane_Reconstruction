1e0b02ae6ecc41231b6f396e863ae466449061ad
Yihao Hu <yihao19@vt.edu>
2025-11-27 20:58:48 -0500

    RHISED002_FlightTest3_2_4_driver

--- 3D_bat_reconstruction/membrane_kinematic_optimization_model/drivers/2024/RHISED002_FlightTest3_2_4_driver.py
+++ 3D_bat_reconstruction/membrane_kinematic_optimization_model/drivers/2024/RHISED002_FlightTest3_2_4_driver.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     test_name = "Brunei_2024_RHISED002_FlightTest3_2_4"
     membrane_simulation_mode = "ANGULAR"
     #[4864, 5120, 5436]
-    start_pose = 5121
-    end_pose = 5436
+    start_pose = 5120
+    end_pose = 5252#5436
     current_pose_index = start_pose
     half_window_size = 8  # animation rendering window size
     membrane_optimized_frame = 1# frame number that will be optimized
@@ -51,8 +51,8 @@ if __name__ == "__main__":
                              template_flip=template_flip
                              )
     
-    driver.run_raw_kinematic_optimize_pipeline()
-    exit(0)
+    #driver.run_raw_kinematic_optimize_pipeline()
+    #exit(0)
     driver.run_kinematic_smoothing()
     #driver.run_membrane_optimize_pipeline(epoch_index = 0)
     #driver.run_membrane_kinematic_update_pipeline(epoch_index=0)
