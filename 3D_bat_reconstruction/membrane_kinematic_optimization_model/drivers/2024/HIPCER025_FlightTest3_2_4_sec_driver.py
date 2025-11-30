5f736a9a01592cc4b26b3fa6fcecc58f830065db
Yihao Hu <yihao19@vt.edu>
2025-11-30 00:18:31 -0500

    HIPCER022_FlightTest1_2_4_sec_driver

--- 3D_bat_reconstruction/membrane_kinematic_optimization_model/drivers/2024/HIPCER025_FlightTest3_2_4_sec_driver.py
+++ 3D_bat_reconstruction/membrane_kinematic_optimization_model/drivers/2024/HIPCER025_FlightTest3_2_4_sec_driver.py
@@ -23,12 +23,12 @@ if __name__ == "__main__":
     current_pose_index = start_pose
     half_window_size = 8  # animation rendering window size
     membrane_optimized_frame = 1# frame number that will be optimized
-    kinematic_opt_epoch = 300
+    kinematic_opt_epoch = 50
     membrane_opt_epoch =100
     membrane_kinematic_opt_epoch = 10
     whole_opt_epoch =1
     if_use_previous_attr = False
-    if_use_previous_kinematics =False
+    if_use_previous_kinematics =True
     opposite_direction = False # bat flying direction
     template_flip = True
     model_template_name = "new_bat_params_version2_backward_membrane_24.pkl"
