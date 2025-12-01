87239d53ac01219a6201302b1019f90d3613c68e
Yihao Hu <yihao19@vt.edu>
2025-12-01 09:28:40 -0500

    Brunei_2024_HIPDYA001_FlightTest3_5_7

--- 3D_bat_reconstruction/membrane_kinematic_optimization_model/drivers/2024/HIPDYA001_FlightTest3_5_7_driver.py
+++ 3D_bat_reconstruction/membrane_kinematic_optimization_model/drivers/2024/HIPDYA001_FlightTest3_5_7_driver.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     test_name = "Brunei_2024_HIPDYA001_FlightTest3_5_7"
     membrane_simulation_mode = "ANGULAR"
     #[3471, 3800, 4087]
-    start_pose = 3799
-    end_pose = 3471
+    start_pose = 3650
+    end_pose = 4087
     current_pose_index = start_pose
     half_window_size = 8  # animation rendering window size
     membrane_optimized_frame = 1# frame number that will be optimized
@@ -54,9 +54,9 @@ if __name__ == "__main__":
                              )
     
       
-    driver.run_raw_kinematic_optimize_pipeline()
+    #driver.run_raw_kinematic_optimize_pipeline()
     #driver.calibration_validation(pose_index=3800)
-    exit(0)
+    #exit(0)
     driver.run_kinematic_smoothing()
     #driver.run_membrane_optimize_pipeline(epoch_index = 0)
     #driver.run_membrane_kinematic_update_pipeline(epoch_index=0)
